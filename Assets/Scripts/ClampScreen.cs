d9354a18f78c62e06a64465105a7fb62a426fa30
Éric Catarina Parreiras <1460909@sga.pucminas.br>
2023-04-17 19:51:56 -0300

    feat: Clamp script creation

---
+++ Assets/Scripts/ClampScreen.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClampScreen : MonoBehaviour
+{
+    private Vector3 pos;
+    // Start is called before the first frame update
+
+
+    void Start()
+    {
+        pos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Mathf.Clamp();
+
+        
+    }
+}
