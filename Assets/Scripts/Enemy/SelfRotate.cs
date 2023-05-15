6818ca5cfeb37f5afa38599aa4c43a8a4fd4a22a
Éric Catarina Parreiras <1460909@sga.pucminas.br>
2023-05-15 19:02:40 -0300

    feat: Shredder creation

---
+++ Assets/Scripts/Enemy/SelfRotate.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class SelfRotate : MonoBehaviour
+{
+    public Vector3 rotation = new Vector3 (0,0,0);
+    void Start()
+    {
+        transform.DORotate(rotation, 5f);
+    }
+
+
+}
